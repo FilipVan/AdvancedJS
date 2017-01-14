dc8ddb2a0cb42c23fafe3f7fc83bf24df9b610ce
FilipVan <vancevski91@gmail.com>
2017-01-14 20:10:34 +0100

    Updated according to lecture
    The code us updated according to hof and iife

--- Advanced_JavaScript/js/script.js
+++ Advanced_JavaScript/js/script.js
@@ -24,49 +24,40 @@ button.addEventListener("click", function () {
                 if (file.readyState === XMLHttpRequest.DONE) {
                     let dataf = JSON.parse(file.responseText);
 
+                 
+      
+               
+                    let makeArray = function (data, operation) {
+                        let result = [];
+                        for (var i = 0; i < data.data.length; i++) {
+                            var item = data.data[i];
+                            result.push(operation(item));
+                        }
+                        return result
+                    }
 
-                    operations(dataf);
+
 
                     function operations(x) {
-                        var rezultat = 0;
                         if (x.operation == "log") {
-                            for (i = 0; i < x.data.length; i++) {
-                                rezultat += Math.log(x.data[i]);
-                            }
-                            result.innerHTML += "The operation 'log' applied to the array " + x.data +
-                                " gives a result of " + rezultat + "<br>";
-                        }
-                        else if (x.operation == "sine") {
-                            var rezultat = 0;
-                            for (i = 0; i < data.length; i++) {
-                                rezultat += Math.sin(x.data[i]);
-                            }
-                            result.innerHTML += "The operation 'sine' applied to the array " + x.data +
-                                " gives a result of " + rezultat + "<br>";
-                        }
-                        else if (x.operation == "cosine") {
-                            var rezultat = 0;
-                            for (i = 0; i < data.length; i++) {
-                                rezultat += Math.cos(x.data[i]);
-                            }
-                            result.innerHTML += "The operation 'cosine' applied to the array " + x.data +
-                                " gives a result of " + rezultat + "<br>";
+                            let log = makeArray(dataf, x => Math.log(x));
+                            console.log(log);
                         }
                         else if (x.operation == "square") {
-                            var rezultat = 0;
-                            for (i = 0; i < data.length; i++) {
-                                rezultat += Math.pow((x.data[i]), 2);
-                            }
-                            result.innerHTML += "The operation 'square' applied to the array " + x.data +
-                                " gives a result of " + rezultat + "<br>";
+                            let square = makeArray(dataf, x => Math.pow(x, 2));
+                            console.log(square);
                         }
-                        else if (x.operation == "cube") {
-                            var rezultat = 0;
-                            for (i = 0; i < data.length; i++) {
-                                rezultat += Math.pow((x.data[i]), 3);
-                            }
-                            result.innerHTML += "The operation 'cube' applied to the array " + x.data +
-                                " gives a result of " + rezultat + "<br>";
+                        else if (x.operation == "sine") {
+                            let sine = makeArray(dataf, x => Math.sin(x));
+                            console.log(sine);
+                        }
+                        else if (x.operation == "sine") {
+                            let cos = makeArray(dataf, x => Math.cos(x));
+                            console.log(cos);
+                        }
+                        else if (x.operation == "sine") {
+                            let cube = makeArray(dataf, x => Math.pow(x, 3));
+                            console.log(cube);
                         }
                     }
                 }
@@ -74,6 +65,65 @@ button.addEventListener("click", function () {
         }
     }
 });
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+
+               //     operations(dataf);
+
+                 //   function operations(x) {
+                   //     var rezultat = 0;
+                     //   if (x.operation == "log") {
+                       //     for (i = 0; i < x.data.length; i++) {
+                         //       rezultat += Math.log(x.data[i]);
+                           // }
+                            //result.innerHTML += "The operation 'log' applied to the array " + x.data +
+                              //  " gives a result of " + rezultat + "<br>";
+                        //}
+                        //else if (x.operation == "sine") {
+                          //  var rezultat = 0;
+                            //for (i = 0; i < data.length; i++) {
+                              //  rezultat += Math.sin(x.data[i]);
+                            //}
+                            //result.innerHTML += "The operation 'sine' applied to the array " + x.data +
+                              //  " gives a result of " + rezultat + "<br>";
+                        //}
+                        //else if (x.operation == "cosine") {
+                          //  var rezultat = 0;
+                           // for (i = 0; i < data.length; i++) {
+                            //    rezultat += Math.cos(x.data[i]);
+                           // }
+                            //result.innerHTML += "The operation 'cosine' applied to the array " + x.data +
+                             //   " gives a result of " + rezultat + "<br>";
+                        //}
+                        //else if (x.operation == "square") {
+                          //  var rezultat = 0;
+                            //for (i = 0; i < data.length; i++) {
+                              //  rezultat += Math.pow((x.data[i]), 2);
+                            //}
+                            //result.innerHTML += "The operation 'square' applied to the array " + x.data +
+                              //  " gives a result of " + rezultat + "<br>";
+                       // }
+                       // else if (x.operation == "cube") {
+                         //   var rezultat = 0;
+                           // for (i = 0; i < data.length; i++) {
+                             //   rezultat += Math.pow((x.data[i]), 3);
+                           // }
+                            //result.innerHTML += "The operation 'cube' applied to the array " + x.data +
+                              //  " gives a result of " + rezultat + "<br>";
+                       // }
+                   // }
+             //   }
+           // }
+       // }
+   // }
+//});
 
 
 
